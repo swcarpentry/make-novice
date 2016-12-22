47d6d15bdf7296ba6bb9fd20e31e602cd30bd40d
Guilherme Castelao <guilherme@castelao.net>
2016-12-21 20:21:52 -0800

    Word count with less I/O, so probably faster.
    Addressing @gcapes comments on #74.

--- code/wordcount.sh
+++ code/wordcount.sh
@@ -4,33 +4,38 @@ inputfile=$1
 outputfile=$2
 min_count=$3
 
-# Clean and normalize the input
+# Clean and normalize the input, one word per line, sorted.
 cat $inputfile \
     | tr "[:punct:]" " " \
     | tr "[:upper:]" "[:lower:]" \
-    > clean.txt
-
-# Create a working temporary directory
-mkdir tmp
+    | tr -cs "[:alpha:]" "\n" \
+    | sort -o clean.txt
 
 # Total number of words
 total=`wc -w clean.txt | awk '{print $1}'`
 
-# Counts the occurence of each word
-for word in `cat clean.txt`
-    do echo '.' >> tmp/$word
+# Be sure to don't reuse tmp.txt
+rm -f tmp.txt
+
+# Counts the occurence of each word.
+count=0
+word=`head -n 1 clean.txt`
+for item in `cat clean.txt`
+    do if [ $word = $item ]
+        then count=$((count + 1))
+    else
+        if [ $count -ge $min_count ]
+	    then echo $count $word >> tmp.txt
+        fi
+	word=$item
+	count=1
+    fi
 done
 
-# Collects the counting into one table and calculate the percentage
-cd tmp
-for word in `ls`
-    do echo `wc -l $word` $total $min_count | \
-    awk '$1>$4{print $1, $2, $1/$3*100}' >> db.txt
-done
+# Order output by most frequent word first, and include the percentage.
+cat tmp.txt \
+    | sort -nr \
+    | awk -v total=$total '{print $2, $1, $1/total*100}' \
+    > $outputfile
 
-# Order the database, the most frequent word first
-cat db.txt | sort -nr | awk '{print $2, $1, $3}' > ../$outputfile
-
-# Clean the working directory
-cd ../
-rm -r tmp
+rm clean.txt tmp.txt
