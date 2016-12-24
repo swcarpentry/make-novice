2d92213eea86d46bee808bfa3ac3966ff1db7a17
Guilherme Castelao <guilherme@castelao.net>
2016-12-24 13:56:00 -0800

    plotcount.sh with aligned bar and fixed screenwidth.

--- code/plotcount.sh
+++ code/plotcount.sh
@@ -9,16 +9,29 @@ else
     limit=10
 fi
 
+screenwidth=80
+gap=2
+
+wordlist=`head -n $limit $inputfile | awk '{print $1}'`
+longestword=`for word in $wordlist; do echo $word | wc -c; done | sort -r | head -n 1`
 max=`head -n 1 $inputfile | awk '{print $2}'`
 
-head $inputfile -n $limit | \
+scale=$((screenwidth - gap - longestword))
+
+
+head -n $limit $inputfile | \
     while read line
         do
             word=`echo $line | awk '{print $1}'`
-	    rank=`echo $line | awk -v max=$max '{print 70*$2/max}'`
-	    bar=''
+	    rank=`echo $line | awk -v max=$max -v scale=$scale '{print scale*$2/max}'`
+	    wlen=`echo $word | wc -c`
+	    extra=$((longestword + gap - wlen))
+	    bar=$word
+	    for i in `seq 1 $extra`
+                do bar+='.'
+            done
 	    for i in `seq 1 $rank`
                 do bar+='#'
             done
-	    echo $word $bar
+	    echo $bar | tr "." " "
         done
