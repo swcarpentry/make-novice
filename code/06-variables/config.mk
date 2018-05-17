89894898ee71752ce19fb19f7ce1d56664b4797a
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-05-17 10:57:05 +0100

    Rename python scripts in each Makefile and config.mk
    Fix #76.

--- code/06-variables/config.mk
+++ code/06-variables/config.mk
@@ -1,8 +1,8 @@
 # Count words script.
 LANGUAGE=python
-COUNT_SRC=wordcount.py
+COUNT_SRC=countwords.py
 COUNT_EXE=$(LANGUAGE) $(COUNT_SRC)
 
 # Test Zipf's rule
-ZIPF_SRC=zipf_test.py
+ZIPF_SRC=testzipf.py
 ZIPF_EXE=$(LANGUAGE) $(ZIPF_SRC)
