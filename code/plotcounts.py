1bc0d6302b0e44b8da29a07e9c3305b38fe955e5
Samuel Lelièvre <slel@users.noreply.github.com>
2019-06-08 14:53:32 +0200

    Future-proof import of Sequence in plotcounts.py
    To keep the lesson working in Python >= 3.8, import `Sequence``
    from `collections.abc` rather than from `collections`. For compatibility,
    use try/except to import from `collections` in earlier versions of Python.
    
    For details on collections.abc vs collections, see [this Stack Overflow question](https://stackoverflow.com/q/55882715).

--- code/plotcounts.py
+++ code/plotcounts.py
@@ -3,7 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-from collections import Sequence
+try:
+    from collections.abc import Sequence
+except ImportError:
+    from collections import Sequence
 
 from countwords import load_word_counts
 
