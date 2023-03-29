3b22eeaaa23e5763cf4d389e56c36799778048c8
Gerard Capes <gerard.capes@manchester.ac.uk>
2023-03-29 13:27:14 +0100

    Propagate pattern rule changes to end of lesson

--- code/09-conclusion-challenge-1/config.mk
+++ code/09-conclusion-challenge-1/config.mk
@@ -1,12 +1,9 @@
 # Count words script.
 LANGUAGE=python
 COUNT_SRC=countwords.py
-COUNT_EXE=$(LANGUAGE) $(COUNT_SRC)
 
 # Plot word counts script.
 PLOT_SRC=plotcounts.py
-PLOT_EXE=$(LANGUAGE) $(PLOT_SRC)
 
 # Test Zipf's rule
 ZIPF_SRC=testzipf.py
-ZIPF_EXE=$(LANGUAGE) $(ZIPF_SRC)
